5cbcd8148a0ca7f4d2ef83cead926ae01d7b12ce
Mark Rodgers <mark@marknrodgers.com>
2022-05-17 01:12:56 -0700

    let there be triangles

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -6,8 +6,8 @@
 #include <SDL_opengl.h>
 #include <gl/GLU.h>
 
-const int SCREEN_WIDTH = 1920;
-const int SCREEN_HEIGHT = 1080;
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 1000;
 
 SDL_Window* gWindow = NULL;
 SDL_GLContext gContext;
@@ -54,7 +54,7 @@ int main(int argc, char* args[]) {
         return -1;
     }
     else {
-        printf("GLEW %s initialized\n", glewGetString(GLEW_VERSION));
+        printf("GLEW %s initialized\n\n", glewGetString(GLEW_VERSION));
     }
 
     // Enable V-Sync
@@ -65,12 +65,75 @@ int main(int argc, char* args[]) {
         }
     }
 
-    // TODO: Create and compile shaders here
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    // Main Loop
+    GLfloat vertices[] = {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+    };
+
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+        "}\0";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 1);
+    glBindVertexArray(0);
+
+    glClearColor(1.f, 1.f, 1.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(gWindow);
+
+    // Application Loop
     SDL_Event event;
-
     while (!quit) {
+        // Graphics
+        glClearColor(1.f, 1.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SDL_GL_SwapWindow(gWindow);
+
+        // Input
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_KEYDOWN:
@@ -87,8 +150,11 @@ int main(int argc, char* args[]) {
                     break;
             }
         }
-        SDL_GL_SwapWindow(gWindow);
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram); 
 
     SDL_DestroyWindow(gWindow);
     SDL_Quit();
